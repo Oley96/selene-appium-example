0cd4e61460c4151528f9337c6a95f44e96982fcc
Vova Oleynik <vladimir@MacBook-Pro-2.local>
2020-10-11 15:43:11 +0300

    Add android support. Split env on ios and android

--- conftest.py
+++ conftest.py
@@ -5,15 +5,26 @@ from selene.api import config
 from appium import webdriver
 import dotenv
 
-dotenv.load_dotenv()
+
+# dotenv.load_dotenv()
+
+def pytest_addoption(parser):
+    parser.addoption("--platform", action="store", default="IOS")
 
 
 @pytest.fixture
-def mobile_driver():
+def mobile_driver(request):
     config.timeout = 10
 
+    if request.config.getoption("--platform") == "ios":
+        dotenv.load_dotenv(dotenv_path=".env.ios")
+    elif request.config.getoption("--platform") == "android":
+        dotenv.load_dotenv(dotenv_path=".env.android")
+    else:
+        raise ValueError("Unsupported cli param")
+
     desired_caps = {
-        'platformName': "IOS",
+        'platformName': os.getenv('platform.name'),
         'platformVersion': os.getenv('platform.version'),
         'deviceName': os.getenv('device.name'),
         'orientation': os.getenv('device.orientation'),
--- screens/main_screen.py
+++ screens/main_screen.py
@@ -1,3 +1,5 @@
+import os
+
 from selene import by, have
 from selene.support.shared.jquery_style import s
 
@@ -5,10 +7,18 @@ from selene.support.shared.jquery_style import s
 class MainScreen:
 
     def __init__(self):
-        self.header = s(by.id("h1Text"))
-        self.comment_text = s(by.id("submittedComments"))
-        self.comment_input = s(by.id("comments"))
-        self.submit_button = s(by.id("submit"))
+
+        if os.getenv('platform.name') == "IOS":
+            self.header = s(by.id("h1Text"))
+            self.comment_text = s(by.id("submittedComments"))
+            self.comment_input = s(by.id("comments"))
+            self.submit_button = s(by.id("submit"))
+
+        elif os.getenv('platform.name') == "ANDROID":
+            self.header = s(by.id("Heading1_1"))
+            self.comment_text = s(by.id("your_comments"))
+            self.comment_input = s(by.id("comments"))
+            self.submit_button = s(by.id("submit"))
 
     def tap_and_type_text_to_comment_input(self, comment):
         self.comment_input.click()
